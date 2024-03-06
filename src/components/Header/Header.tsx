dc44e3947866db13b92dfcea4696547dfd4f441c
TrinhTuan <trinhtuan2801@gmail.com>
2024-03-06 18:16:31 +0700

    header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,25 @@
 import { Box, Typography } from '@mui/joy';
-
+import './index.scss';
 export default function Header() {
   return (
     <Box
-      position='sticky'
+      // position='sticky'
       top={0}
       left={0}
-      py={1}
+      py={2}
       px={2}
       zIndex={1100}
-      width='fit-content'
     >
       <Typography
-        textColor='common.white'
-        level='title-lg'
         fontSize='24px'
         fontWeight='bold'
         fontFamily='var(--font-playpen-sans)'
-        sx={{
-          textShadow: 'rgba(0, 0, 0, 0.5) 0px 0px 2px',
-        }}
+        textAlign='center'
+        className='header-text playful'
       >
-        Lubby Cake
+        {'Lubby Cake'.split('').map((char, index) => (
+          <span key={index}>{char}</span>
+        ))}
       </Typography>
     </Box>
   );
