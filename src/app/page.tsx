b5c3d0509115bd0887f5b1fc09b93ea05910233e
TrinhTuan <trinhtuan2801@gmail.com>
2024-03-08 14:55:03 +0700

    change folder name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getCakes } from '@/api/cake';
-import FilterContainer from '@/components/FilterContainer.tsx/FilterContainer';
+import FilterContainer from '@/components/FilterContainer/FilterContainer';
 
 export default async function Home() {
   const cakes = await getCakes();
