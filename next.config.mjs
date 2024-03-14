46760be3b59615c24d23dd1c659a92c67b826018
TrinhTuan <trinhtuan2801@gmail.com>
2024-03-14 13:40:29 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
 import withPlaiceholder from '@plaiceholder/next';
 
+const getCorsHeaders = () => {
+  const headers = {};
+
+  headers['Access-Control-Allow-Origin'] =
+    'https://lubby-cake-admin.vercel.app';
+  headers['Access-Control-Allow-Credentials'] = 'true';
+  headers['Access-Control-Allow-Methods'] = 'GET,OPTIONS,PATCH,DELETE,POST,PUT';
+  headers['Access-Control-Allow-Headers'] =
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization';
+
+  return Object.entries(headers).map(([key, value]) => ({ key, value }));
+};
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -9,6 +22,14 @@ const nextConfig = {
         hostname: 'firebasestorage.googleapis.com',
       },
     ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: getCorsHeaders(),
+      },
+    ];
   },
 };
 
