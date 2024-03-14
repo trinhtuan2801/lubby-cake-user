8b3fd30161c02a049ac248afcb1b20ed8bdb5d4e
TrinhTuan <trinhtuan2801@gmail.com>
2024-03-14 16:47:20 +0700

    update cors rule

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const allowedOrigins = ['https://lubby-cake-admin.vercel.app/'];
-
-const corsOptions = {
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-};
+const allowedOrigins = ['https://lubby-cake-admin.vercel.app'];
 
 export function middleware(request: NextRequest) {
-  // Check the origin from the request
-  const origin = request.headers.get('origin') ?? '';
-  const isAllowedOrigin = allowedOrigins.includes(origin);
+  const origin = request.headers.get('origin');
+
   // eslint-disable-next-line
   console.log('origin', origin);
-  // Handle preflighted requests
-  const isPreflight = request.method === 'OPTIONS';
+  const isAllowedOrigin = origin && allowedOrigins.includes(origin);
 
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
+  if (!isAllowedOrigin) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: 'Bad Request',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    });
   }
 
-  // Handle simple requests
-  const response = NextResponse.next();
-
-  if (isAllowedOrigin) {
-    response.headers.set('Access-Control-Allow-Origin', origin);
-  }
-
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
-
-  return response;
+  return NextResponse.next();
 }
 
 export const config = {
